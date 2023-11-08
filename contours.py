d57c771caa135f8b7faf7045c863b48b98568766
moiznoorani <nooranimoiz52@gmail.com>
2023-11-08 10:00:00 -0800

    commit

--- contours.py
+++ contours.py
@@ -15,13 +15,17 @@ cv.imshow("Gray", gray)
 
 canny = cv.Canny(img, 125,175)
 cv.imshow("Canny Edges", canny)
+
 ret, thresh = cv.threshold(gray,125,255, cv.THRESH_BINARY)
 cv.imshow("Thresh", thresh)
+
 # contours python list of all coordinates of contours found in the image and hierarchies representation of contours
 contours, hierarchies = cv.findContours(canny,cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
 print(f'{len(contours)} contours found!')
+
 cv.drawContours(blank,contours, -1 , (0,0,255), 1)
 cv.imshow("Contours Drawn", blank)
+
 
 
 cv.waitKey(0)
---
+++ spaces.py
@@ -0,0 +1,41 @@
+import cv2 as cv
+import matplotlib.pyplot as plt 
+
+img = cv.imread('photos/park.jpg')
+cv.imshow('park',img)
+# plt.imshow(img)
+# plt.show()
+
+# BGR to Grayscale
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray', gray)
+
+#BGR to HSV
+
+hsv = cv.cvtColor(img,cv.COLOR_BGR2HSV)
+cv.imshow("HSV", hsv)
+
+# BGR to LAB or l*a*b
+lab = cv.cvtColor(img, cv.COLOR_BGR2Lab)
+cv.imshow("LAB", lab)
+
+# BGR to RGB
+rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+cv.imshow("RGB", rgb)
+
+#HSV to BGR
+
+HSV_BGR = cv.cvtColor(hsv,cv.COLOR_HSV2BGR)
+cv.imshow("HSV_to_BGR", HSV_BGR)
+
+#LAB to BGR
+LAB_BGR = cv.cvtColor(lab,cv.COLOR_Lab2BGR)
+cv.imshow("LAB_to_BGR", LAB_BGR)
+
+plt.imshow(rgb)
+plt.show()
+
+
+
+cv.waitKey(0)
+
